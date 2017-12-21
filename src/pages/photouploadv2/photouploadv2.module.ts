526dff4f08845ea7c9ea1fa7b57b7dd033a70202
erginipekci7 <erginipekci7@gmail.com>
2017-12-22 00:30:38 +0300

    Api'ye foto atildi

---
+++ src/pages/photouploadv2/photouploadv2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Photouploadv2Page } from './photouploadv2';
+
+@NgModule({
+  declarations: [
+    Photouploadv2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Photouploadv2Page),
+  ],
+})
+export class Photouploadv2PageModule {}
