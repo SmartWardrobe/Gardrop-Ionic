fef19caa5e5b2ed42684c0d89b6bfb5df22acf6a
erginipekci7 <erginipekci7@gmail.com>
2017-12-30 11:58:36 +0300

    Ionic te havadurumu servisi oluşturuldu

---
+++ src/providers/weatherservice/weatherservice.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http,Response } from '@angular/http' ;
+import 'rxjs/add/operator/map';
+
+
+//flasktan havadurumu verileri bu servis ile cekilecek.
+
+@Injectable()
+export class WeatherserviceProvider {
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherserviceProvider Provider');
+  }
+  getWeather(){
+   return this.http.get("https://gardrop-api.herokuapp.com/v1/temperature/istanbul")
+   .map(this.extractData)
+  }
+  private extractData(res: Response){
+    return res.json();
+  }
+
+}
