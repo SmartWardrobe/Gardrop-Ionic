8ae2a64f33524b6e14769e2a3884c451378f9a70
erginipekci7 <erginipekci7@gmail.com>
2018-01-06 11:31:28 +0300

    login kontrolu yapıldı

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http, Headers, RequestOptions } from '@angular/http'; 
 
 
 
@@ -9,8 +10,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  data:any = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  this.data.email = '';
+  this.data.password = '';
+
+  this.http = http;
+  }
+
+  login(){
+    let headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append("Content-type", 'application/json');
+
+    let options = new RequestOptions({ headers: headers });
+
+    let link = 'https://gardrop-api.herokuapp.com/v1/login';
+    let myData = {
+      "email" : this.data.email,
+      "password" : this.data.password
+    };
+    console.log(myData);
+    this.http.post(link,myData,options)
+      .subscribe(data => {
+        this.data.response = data["status"]; // 200 donduruyor
+        if (this.data.response === 200){
+          this.data.response="Giriş Başarılı"
+        }
+        //yeni sayfaya yonlendirecek...
+      },error => {
+          console.log("Erorrr!");
+      });
+
   }
 
   ionViewDidLoad() {
