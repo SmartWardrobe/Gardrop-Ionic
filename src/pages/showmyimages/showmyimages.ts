1adb72909954d1344b42e940038e64c6b522cad1
pleycpl <h4rvey@protonmail.com>
2018-01-07 11:51:22 +0000

    gereksiz kisimlar kaldirildi, ve global'den user_info cekiliyor.

--- src/pages/showmyimages/showmyimages.ts
+++ src/pages/showmyimages/showmyimages.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GlobalProvider } from "../../providers/global/global";
 
-
-
 @IonicPage()
 @Component({
   selector: 'page-showmyimages',
@@ -11,17 +9,13 @@ import { GlobalProvider } from "../../providers/global/global";
 })
 export class ShowmyimagesPage {
   galleryType = 'multi';
-  userinfo: any;
   pics: any;
+
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public global: GlobalProvider) {
-    //this.userinfo = navParams.get('userinfo');
-    this.global.global_user_info = navParams.get('userinfo');
-    //console.log(this.userinfo);
-    console.log(this.global.global_user_info);
-    //this.pics = this.userinfo.pics;
-    this.pics = this.global.global_user_info;
+    console.log(this.global.user_info);
+    this.pics = this.global.user_info;
     console.log(`Pics: ${this.pics}`)
     for (let pic of this.pics) {
       console.log(`Pic: ${pic}`)
