052e1ff1a972604a461e2dc48d2001f4f338f18e
erginipekci7 <erginipekci7@gmail.com>
2018-01-07 14:01:17 +0300

    Key duzeltildi.

--- src/pages/photoproperties/photoproperties.ts
+++ src/pages/photoproperties/photoproperties.ts
@@ -19,7 +19,7 @@ export class PhotopropertiesPage {
   }
   updatePhotoProperties() {
     let myPhotoData = {
-      "photoname" : this.filename,
+      "filename" : this.filename,
       "color" : this.photocolor
     };
     this.http.put("https://gardrop-api.herokuapp.com/v1/pic", myPhotoData)
