187d01f94c1b9cf184bef0d7d4217011af67e0c7
pleycpl <h4rvey@protonmail.com>
2018-01-07 17:25:39 +0000

    hava kismi buraya eklendi, ve navbar kaldirildi.

--- src/pages/showmyimages/showmyimages.ts
+++ src/pages/showmyimages/showmyimages.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GlobalProvider } from "../../providers/global/global";
 import { PhotouploadPage } from '../photoupload/photoupload';
+import { WeatherserviceProvider } from '../../providers/weatherservice/weatherservice';
 
 @IonicPage()
 @Component({
@@ -11,15 +12,20 @@ import { PhotouploadPage } from '../photoupload/photoupload';
 export class ShowmyimagesPage {
   galleryType = 'multi';
   pics: any;
+  weatherType:any;
+  weatherDegree:any;
+  iconType:any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public global: GlobalProvider) {
+              public global: GlobalProvider,
+              public weatherService: WeatherserviceProvider) {
     console.log(this.global.user_info);
     this.pics = this.global.user_info.pics;
     for (let pic of this.pics) {
       console.log(`Pic: Filename:${pic.filename} Color:${pic.color} Type:${pic.type}`)
     }
+    this.loadWeather();
   }
 
   openPhotouploadPage(){
@@ -29,5 +35,27 @@ export class ShowmyimagesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShowmyimagesPage');
   }
+  loadWeather(){
+    this.weatherService.getWeather()
+    .subscribe(data => {
+      this.weatherDegree = data.content.main.temp_max - 273.15;
+      this.weatherType = data.content.weather[0].main;
+      this.showWeatherType();
+    });
+  }
 
+  showWeatherType(){
+    if(this.weatherType==='Rain'){
+      console.log("weather is rain")
+      this.iconType="ios-rainy-outline";
+    }else if(this.weatherType==='Sunny' || this.weatherType==='Clear'){
+      this.iconType="ios-sunny-outline";
+    }else if(this.weatherType==='Snow'){
+      this.iconType="ios-snow-outline";
+    }else if(this.weatherType==='Clouds'){
+      this.iconType="ios-cloud-outline";
+    }else if(this.weatherType === 'Mist'){
+      this.iconType="ios-barcode-outline";
+    }
+  }
 }
